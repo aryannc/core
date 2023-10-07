cae44513db78c0f7b98a9fadacfaf33985c1f234
Aryan NC <10281988+aryannc@users.noreply.github.com>
2023-10-08 01:25:45 +0200

    Unused function parameters in sensor.py removed
    Unused parameters are misleading. Whatever the value passed to such parameters is, the behaviour will be the same.

--- homeassistant/components/sigfox/sensor.py
+++ homeassistant/components/sigfox/sensor.py
@@ -12,10 +12,9 @@ import voluptuous as vol
 
 from homeassistant.components.sensor import PLATFORM_SCHEMA, SensorEntity
 from homeassistant.const import CONF_NAME
-from homeassistant.core import HomeAssistant
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+from homeassistant.helpers.typing import ConfigType
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -35,10 +34,8 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
 
 
 def setup_platform(
-    hass: HomeAssistant,
     config: ConfigType,
-    add_entities: AddEntitiesCallback,
-    discovery_info: DiscoveryInfoType | None = None,
+    add_entities: AddEntitiesCallback
 ) -> None:
     """Set up the sigfox sensor."""
     api_login = config[CONF_API_LOGIN]
