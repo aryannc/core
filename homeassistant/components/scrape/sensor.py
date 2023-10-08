41c3b2b1b2a25570a4759ea57c78df0d18655a33
Aryan NC <10281988+aryannc@users.noreply.github.com>
2023-10-08 02:37:37 +0200

    Unused function parameters in sensor.py removed
    Unused parameters are misleading. Whatever the value passed to such parameters is, the behaviour will be the same.

--- homeassistant/components/scrape/sensor.py
+++ homeassistant/components/scrape/sensor.py
@@ -51,7 +51,6 @@ TRIGGER_ENTITY_OPTIONS = (
 
 async def async_setup_platform(
     hass: HomeAssistant,
-    config: ConfigType,
     async_add_entities: AddEntitiesCallback,
     discovery_info: DiscoveryInfoType | None = None,
 ) -> None:
