a3e455d76a73ac8c4f28d466565e1d51aff12ac2
Aryan NC <10281988+aryannc@users.noreply.github.com>
2023-10-08 03:20:03 +0200

    Unused function parameters in diagnostics.py removed
    Unused parameters are misleading. Whatever the value passed to such parameters is, the behaviour will be the same.

--- homeassistant/components/rfxtrx/diagnostics.py
+++ homeassistant/components/rfxtrx/diagnostics.py
@@ -5,13 +5,12 @@ from typing import Any
 
 from homeassistant.components.diagnostics import async_redact_data
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
 
 TO_REDACT = {"host"}
 
 
 async def async_get_config_entry_diagnostics(
-    hass: HomeAssistant, entry: ConfigEntry
+    entry: ConfigEntry
 ) -> dict[str, Any]:
     """Return diagnostics for a config entry."""
     return {
