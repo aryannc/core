f9f9bdeeccb52b8bb3837f94d9fe8472cb140ec4
Aryan NC <10281988+aryannc@users.noreply.github.com>
2023-10-08 03:41:36 +0200

    Unused function parameters in  __init__.py removed
    Unused parameters are misleading. Whatever the value passed to such parameters is, the behaviour will be the same.

--- homeassistant/components/repairs/__init__.py
+++ homeassistant/components/repairs/__init__.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import config_validation as cv
-from homeassistant.helpers.typing import ConfigType
 
 from . import issue_handler, websocket_api
 from .const import DOMAIN
@@ -29,7 +28,7 @@ def repairs_flow_manager(hass: HomeAssistant) -> RepairsFlowManager | None:
     return flow_manager
 
 
-async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+async def async_setup(hass: HomeAssistant) -> bool:
     """Set up Repairs."""
     hass.data[DOMAIN] = {}
 
