8e10224eef1fec017f9921fad059805a6143a061
Aryan NC <10281988+aryannc@users.noreply.github.com>
2023-10-08 03:37:46 +0200

    Unused function parameters in  issue_handler.py removed
    Unused parameters are misleading. Whatever the value passed to such parameters is, the behaviour will be the same.

--- homeassistant/components/repairs/issue_handler.py
+++ homeassistant/components/repairs/issue_handler.py
@@ -24,7 +24,7 @@ class ConfirmRepairFlow(RepairsFlow):
     """Handler for an issue fixing flow without any side effects."""
 
     async def async_step_init(
-        self, user_input: dict[str, str] | None = None
+        self
     ) -> data_entry_flow.FlowResult:
         """Handle the first step of a fix flow."""
         return await self.async_step_confirm()
