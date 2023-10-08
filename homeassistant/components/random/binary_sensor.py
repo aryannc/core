eace5ea62e1927e7ab6b9c137765b8f6566afad4
Aryan NC <10281988+aryannc@users.noreply.github.com>
2023-10-08 04:03:34 +0200

    Unused function parameters in binary_sensor.py removed
    Unused parameters are misleading. Whatever the value passed to such parameters is, the behaviour will be the same.

--- homeassistant/components/random/binary_sensor.py
+++ homeassistant/components/random/binary_sensor.py
@@ -14,7 +14,7 @@ from homeassistant.const import CONF_DEVICE_CLASS, CONF_NAME
 from homeassistant.core import HomeAssistant
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+from homeassistant.helpers.typing import ConfigType
 
 DEFAULT_NAME = "Random Binary Sensor"
 
@@ -27,10 +27,8 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
 
 
 async def async_setup_platform(
-    hass: HomeAssistant,
     config: ConfigType,
     async_add_entities: AddEntitiesCallback,
-    discovery_info: DiscoveryInfoType | None = None,
 ) -> None:
     """Set up the Random binary sensor."""
     name = config.get(CONF_NAME)
