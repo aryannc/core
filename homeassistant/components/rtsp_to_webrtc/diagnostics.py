1c3d6f569ad76bdef7f3e6972806392dc19e3706
Aryan NC <10281988+aryannc@users.noreply.github.com>
2023-10-08 02:53:01 +0200

    Unused function parameters in diagnostics.py removed
    Unused parameters are misleading. Whatever the value passed to such parameters is, the behaviour will be the same.

--- homeassistant/components/rtsp_to_webrtc/diagnostics.py
+++ homeassistant/components/rtsp_to_webrtc/diagnostics.py
@@ -6,12 +6,7 @@ from typing import Any
 
 from rtsp_to_webrtc import client
 
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
 
-
-async def async_get_config_entry_diagnostics(
-    hass: HomeAssistant, config_entry: ConfigEntry
-) -> dict[str, Any]:
+async def async_get_config_entry_diagnostics() -> dict[str, Any]:
     """Return diagnostics for a config entry."""
     return dict(client.get_diagnostics())
