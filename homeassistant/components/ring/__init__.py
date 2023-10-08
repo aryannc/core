8b2641d9fef87d867da952af3bb7df2b274c5c6c
Aryan NC <10281988+aryannc@users.noreply.github.com>
2023-10-08 03:17:44 +0200

    Unused function parameters in  __init__.py removed
    Unused parameters are misleading. Whatever the value passed to such parameters is, the behaviour will be the same.

--- homeassistant/components/ring/__init__.py
+++ homeassistant/components/ring/__init__.py
@@ -129,9 +129,7 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     return True
 
 
-async def async_remove_config_entry_device(
-    hass: HomeAssistant, config_entry: ConfigEntry, device_entry: dr.DeviceEntry
-) -> bool:
+async def async_remove_config_entry_device() -> bool:
     """Remove a config entry from a device."""
     return True
 
